6448c4ba375fce5cea1024608b29d378d575b44e
QWERTY <nio07212000@mail.ru>
2021-04-26 22:26:00 +0300

    Just start ;)

---
+++ src/main/java/com/alekseev/rest/dins/phone_book/StartMyPhoneBookApplication.java
@@ -0,0 +1,11 @@
+package com.alekseev.rest.dins.phone_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartMyPhoneBookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartMyPhoneBookApplication.class, args);
+    }
+}
